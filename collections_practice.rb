2484271f60d3ce8888a6fe00f52720c745bfe380
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-27 00:39:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,10 +9,11 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a, b|
-    a.length <=> b.length 
+    a.length <=> b.length
   end
 end
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
-end 
+  return array 
+end
