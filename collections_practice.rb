311c9f9df160b91aa018b09ec2340bc5295e86ca
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-27 00:31:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,9 +1,13 @@
 def sort_array_asc(array)
-  array.sort 
+  array.sort
 end
 
 def sort_array_desc(array)
   new_array = sort_array_asc(array)
   new_array.reverse!
-end 
+end
 
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a <=> b
+end 
